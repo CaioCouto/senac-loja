182d5ef3211503da64a8fcd1d860da59741c2eef
Caio Couto <coutocaio3@gmail.com>
2021-11-04 08:18:09 -0300

    Alternando a conexão para o driver SQLite

--- php/classes/DatabaseConfig.php
+++ php/classes/DatabaseConfig.php
@@ -4,49 +4,91 @@ class DatabaseConfig {
     protected $dbh;
 
     protected function __construct() {
-        // $root = dirname(dirname(__DIR__));
-        // $this->dbh = new PDO('sqlite:'.$root);
-        $this->dbh = new PDO('mysql:host=localhost;dbname=loja', 'root', '');
+        $root = dirname(dirname(__DIR__));
+        $this->dbh = new PDO("sqlite:{$root}/loja.sqlite");
+        // $this->dbh = new PDO('mysql:host=localhost;dbname=loja', 'root', '');
         $this->createUsersTable();
         $this->createProductsTable();
         $this->createShoppingcartTable();
     }
-    
+
+    // Trecho de código relativo à conexão via SQLite
     private function createUsersTable() {
         $this->dbh->exec('CREATE TABLE IF NOT EXISTS users (
-            name VARCHAR(25),
-            surname VARCHAR(100), 
-            cpf CHAR(14),
-            gender VARCHAR(2),
-            email VARCHAR(60) PRIMARY KEY,
-            password VARCHAR(255),
-            cep VARCHAR(9),
-            city VARCHAR(50),
-            address VARCHAR(100),
-            houseNumber VARCHAR(5),
-            neighborhood VARCHAR(50),
-            status VARCHAR(6) DEFAULT "user"
+            name TEXT,
+            surname TEXT, 
+            cpf TEXT,
+            gender TEXT,
+            email TEXT PRIMARY KEY,
+            password TEXT,
+            cep TEXT,
+            city TEXT,
+            address TEXT,
+            houseNumber TEXT,
+            neighborhood TEXT,
+            status TEXT DEFAULT "user"
         )');
     }
 
     private function createProductsTable() {
         $this->dbh->exec('CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTO_INCREMENT,
-            name VARCHAR(255), 
-            value DECIMAL(10, 2),
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT, 
+            value REAL,
             quantity INTEGER UNSIGNED,
-            category VARCHAR(50),
-            description VARCHAR(65535),
-            image VARCHAR(255)
+            category TEXT,
+            description TEXT,
+            image TEXT
         )');
     }
 
     private function createShoppingcartTable() {
         $this->dbh->exec('CREATE TABLE IF NOT EXISTS shoppingcarts (
             product_id INTEGER,
-            user_email VARCHAR(60),
+            user_email TEXT,
             FOREIGN KEY(product_id) REFERENCES Products(id),
             FOREIGN KEY(user_email) REFERENCES Users(email)
         )');
+    } 
+
+    // Trecho de código relativo à conexão via MySQL
+    /* 
+    private function createUsersTable() {
+        $this->dbh->exec('CREATE TABLE IF NOT EXISTS users (
+            name TEXT,
+            surname TEXT, 
+            cpf CHAR(14),
+            gender TEXT,
+            email TEXT PRIMARY KEY,
+            password TEXT,
+            cep TEXT,
+            city TEXT,
+            address TEXT,
+            houseNumber TEXT,
+            neighborhood TEXT,
+            status TEXT DEFAULT "user"
+        )');
     }
+
+    private function createProductsTable() {
+        $this->dbh->exec('CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            name TEXT, 
+            value DECIMAL(10, 2),
+            quantity INTEGER UNSIGNED,
+            category TEXT,
+            description TEXT,
+            image TEXT
+        )');
+    }
+
+    private function createShoppingcartTable() {
+        $this->dbh->exec('CREATE TABLE IF NOT EXISTS shoppingcarts (
+            product_id INTEGER,
+            user_email TEXT,
+            FOREIGN KEY(product_id) REFERENCES Products(id),
+            FOREIGN KEY(user_email) REFERENCES Users(email)
+        )');
+    } 
+    */
 }
